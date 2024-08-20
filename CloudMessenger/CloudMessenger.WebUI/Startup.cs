6fa321b577a5941e4c193fd7dd0bac5591c08061
Daniel Peralba <81268953+danielcperalba@users.noreply.github.com>
2024-08-20 15:07:08 -0300

    Project update

--- CloudMessenger/CloudMessenger.WebUI/Startup.cs
+++ CloudMessenger/CloudMessenger.WebUI/Startup.cs
@@ -1,13 +1,10 @@
+using CloudMessenger.WebUI.Hubs;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using CloudMessenger.WebUI;
 
 namespace CloudMessenger.WebUI
 {
@@ -24,6 +21,7 @@ namespace CloudMessenger.WebUI
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+            services.AddSignalR();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -51,6 +49,7 @@ namespace CloudMessenger.WebUI
                 endpoints.MapControllerRoute(
                     name: "default",
                     pattern: "{controller=Home}/{action=Index}/{id?}");
+                endpoints.MapHub<ChatHub>("/chatHub");
             });
         }
     }
